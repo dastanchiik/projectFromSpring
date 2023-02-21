33cd306d2c42d8e70bffd3b6a118123e99fbdd42
dastan <dosyafree32@gmail.com>
2023-02-21 18:24:30 +0600

    right

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,1 +1,12 @@
-package org.example;import org.springframework.context.ApplicationContext;import org.springframework.context.support.ClassPathXmlApplicationContext;public class App { public static void main(String[] args) {ApplicationContext ap = new ClassPathXmlApplicationContext("s.xml");Person person = (Person) ap.getBean("person");System.out.println(person);}}
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext ap = new ClassPathXmlApplicationContext("s.xml");
+        Person person = (Person) ap.getBean("person");
+        System.out.println(person);
+    }
+}
