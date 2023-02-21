deae2cb92b5cc2b5396695f3d16fcd0d8160d5d1
dastan <dosyafree32@gmail.com>
2023-02-21 18:24:06 +0600

    wrong

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,1 @@
-package org.example;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-public class App {
-    public static void main(String[] args) {
-        ApplicationContext ap = new ClassPathXmlApplicationContext("s.xml");
-        Person person = (Person) ap.getBean("person");
-        System.out.println(person);
-    }
-}
+package org.example;import org.springframework.context.ApplicationContext;import org.springframework.context.support.ClassPathXmlApplicationContext;public class App { public static void main(String[] args) {ApplicationContext ap = new ClassPathXmlApplicationContext("s.xml");Person person = (Person) ap.getBean("person");System.out.println(person);}}
