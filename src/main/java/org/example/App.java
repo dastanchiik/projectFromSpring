d8279d1bbc34e9a0c10bf091c60e9434b93188ec
dastan <dosyafree32@gmail.com>
2023-02-22 23:01:26 +0600

    qwerty

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,1 @@
-package org.example;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class App {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);
-        Person person = (Person) ac.getBean("person");
-        System.out.println(person);
-    }
-}
+package org.example;import org.springframework.context.annotation.AnnotationConfigApplicationContext;public class App { public static void main(String[] args) {AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(Config.class);Person person = (Person) ac.getBean("person");System.out.println(person);}}
